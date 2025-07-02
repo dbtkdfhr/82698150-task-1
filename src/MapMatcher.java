88a834d650b41e166ce4d9a1393e5c731b20e7bc
dbtkdfhr2000 <dbtkdfhr2000@kookmin.ac.kr>
2025-07-03 00:35:36 +0900

    feat: 각도 평준화 알고리즘 추가

--- src/MapMatcher.java
+++ src/MapMatcher.java
@@ -7,11 +7,27 @@ public class MapMatcher {
     List<Way> ways;
     List<GPSPoint> points;
     Set<Long> routeWayIds;
+    
+    List<Long> matchedWayIds = new ArrayList<>();
+    List<Boolean> isDeviationList = new ArrayList<>();
+
+    static final double MAX_DIST = 40.0;
+    static final double MAX_ANGLE_DIFF = 45.0;
 
     public MapMatcher(Map<Long, Node> nodes, List<Way> ways, List<GPSPoint> points, List<Long> routeWayIds) {
         this.nodes = nodes;
         this.ways = ways;
         this.points = points;
         this.routeWayIds = new HashSet<>(routeWayIds);
+    }
+
+    private double normalizeAngle(double deg) {
+        deg = deg % 360.0;
+
+        if (deg > 180.0) deg -= 360.0;
+
+        if (deg < -180.0) deg += 360.0;
+
+        return deg;
     }
 }
